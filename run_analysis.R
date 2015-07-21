20f9132724c3057d3ed786a674978b9efb52400c
gpsabp <a.pimentel@campus.fct.unl.pt>
2015-07-21 13:58:15 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 library(data.table)
 
-
 #read train and test info
 train <- read.table("./UCI HAR Dataset//train//X_train.txt")
 trainActivity <- read.table("./UCI HAR Dataset//train//y_train.txt")
@@ -11,15 +10,18 @@ testActivity <- read.table("./UCI HAR Dataset//test//y_test.txt")
 testSubject <- read.table("./UCI HAR Dataset//test//subject_test.txt")
 
 features <- read.table("./UCI HAR Dataset//features.txt")
+features[,2] <- tolower(features[,2])
+features[,2] <- gsub("[-()]","",features[,2],)
+
 
 #rename labels
 colnames(train) <- features[,2]
-colnames(trainActivity)[1] <- "Activity" 
-colnames(trainSubject)[1] <- "Subject"
+colnames(trainActivity)[1] <- "activity" 
+colnames(trainSubject)[1] <- "subject"
 
 colnames(test) <- features[,2]
-colnames(testActivity)[1] <- "Activity" 
-colnames(testSubject)[1] <- "Subject"
+colnames(testActivity)[1] <- "activity" 
+colnames(testSubject)[1] <- "subject"
 
 #bind Activity and Subject to train and test
 dataTrain <- cbind(train, trainActivity, trainSubject)
@@ -29,14 +31,14 @@ dataTest <- cbind(test, testActivity, testSubject)
 data <- rbind(dataTrain,dataTest)
 
 #select measures with mean and std - also include activity and subject labels
-dataTidy <- data[grepl("mean",colnames(data)) | grepl("Mean",colnames(data)) | grepl("std",colnames(data)) | grepl("Std",colnames(data)) | colnames(data)=="Activity" | colnames(data)=="Subject"]
+dataTidy <- data[grepl("mean",colnames(data))  | grepl("std",colnames(data)) | colnames(data)=="activity" | colnames(data)=="subject"]
 
 #mean of all variables groubed by activity and subject
-dataTidy2 <- aggregate(dataTidy[, 1:86], list(dataTidy$Activity,dataTidy$Subject), mean)
+dataTidy2 <- aggregate(dataTidy[, 1:86], list(dataTidy$activity,dataTidy$subject), mean)
 
 #renames groups
-colnames(dataTidy2)[1] <- "Activity" 
-colnames(dataTidy2)[2] <- "Subject" 
+colnames(dataTidy2)[1] <- "activity" 
+colnames(dataTidy2)[2] <- "subject" 
 
 #write to txt file
 write.table(dataTidy2, file="tidyData.txt",row.name=FALSE)
